2e83d511ecdbc7729474fd9064cc2a1392a3932b
Ethan McCue <ethan@mccue.dev>
2023-09-25 23:11:32 -0400

    Update env

--- src/main/java/dev/mccue/microhttp/realworld/Env.java
+++ src/main/java/dev/mccue/microhttp/realworld/Env.java
@@ -2,12 +2,14 @@ package dev.mccue.microhttp.realworld;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.util.Objects;
+
 public final class Env {
     private Env() {}
 
     private static final Dotenv DOTENV = Dotenv.load();
 
-    public static final String JWT_SECRET = DOTENV.get("JWT_SECRET");
+    public static final String JWT_SECRET = Objects.requireNonNull(DOTENV.get("JWT_SECRET"));
     public static final int PORT = Integer.parseInt(DOTENV.get("PORT", "5555"));
-    public static final String ENVIRONMENT = DOTENV.get("ENVIRONMENT");
+    public static final String ENVIRONMENT = Objects.requireNonNull(DOTENV.get("ENVIRONMENT"));
 }
